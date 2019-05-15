af213b84e5299422991949fb9a3204a9ea2a9068
polarode <40320189+polarode@users.noreply.github.com>
2019-05-15 17:15:30 +0200

    Added some comments

--- Simon-says.asm
+++ Simon-says.asm
@@ -6,36 +6,45 @@ LJMP isr_timer0
 
 ORG 0100h
 init:
-MOV R0, #0d
-MOV TMOD, #00000010b
-MOV TL0, #0d
-MOV TH0, #0d
-SETB IT0
-SETB TR0
-SJMP haupt
+MOV R0, #0d		; Startwert für Zufallszahlen mit 0 initialisieren
+MOV TMOD, #00000010b	; Timermodus 8-Bit-Timer mit Autoreload auf Timer0
+MOV TL0, #0d		; Startwert Timer
+MOV TH0, #0d		; Nachladewert Timer
+SETB IT0		; fallende Taktflanke
+SETB TR0		; starte Timer
+SJMP haupt		; springe zum Hauptprogramm
 
 haupt:
-JNB P1.0, haupt
-CLR TR0
-LCALL anzeigen
-LCALL einlesen
+JNB P1.0, haupt		; mache nichts, bis Taster P1.0 gedrückt wird
+CLR TR0			; stoppe Timer
+LCALL anzeigen		; Muster anzeigen
+LCALL einlesen		; Eingaben des Nutzers einlesen
+LCALL ergebnis		; Eingaben mit tatsächlichem Muster vergleichen und entsprechend Ergebnis darstellen
+SJMP haupt
 
 anzeigen:
-LCALL zufall
-MOV A, R0
-ANL A, #00001111b
-MOV DPTR, #tabelle
-MOVC A, @A+DPTR
-MOV P0, A
-LCALL warte
+MOV R1, #4d		; initialisiere R1 mit 4 (es sollen 4 verschiedene Muster angezeigt werden)
+s1:
+LCALL zufall		; aus dem Startwert eine Zufallszahl generieren
+MOV A, R0		; lade Zufallszahl in den Akku
+ANL A, #00001111b	; maskiere Akku, um eine zufällige Zahl zwischen 0 und 7 zu erhalten
+MOV DPTR, #tabelle	; Datenpointer auf tabelle legen
+MOVC A, @A+DPTR		; hole das entsprechende Bit-Muster aus der Tabelle
+MOV P0, A		; zeige das Bit-Muster auf P0 an
+LCALL warte		; warte
+DJNZ R1, s1		; springe zu s1, wenn noch nicht 4 Muster angezeigt wurden
 RET
 
 einlesen:
 ;todo
 RET
 
+ergebnis:
+;todo
+RET
+
 isr_timer0:
-INC R0
+INC R0			; Generierung Startwert für Zufallszahl
 RETI
 
 warte:
@@ -43,17 +52,17 @@ warte:
 RET
 
 zufall:
-mov A, R0   ; initialisiere A mit R0
-jnz zub
-cpl A
-mov R0, A
+MOV A, R0		; initialisiere A mit R0
+JNZ zub			; Startwert darf nicht 0 sein
+CPL A			; complementiere, falls doch 0
+MOV R0, A
 zub:
-anl A, #10111000b
-mov C, P
-mov A, R0
-rlc A
-mov R0, A
-ret
+ANL A, #10111000b	; 
+MOV C, P		;
+MOV A, R0		;
+RLC A			;
+MOV R0, A		;
+RET
 
 tabelle:
 DB 00000001b, 00000010b, 00000100b, 00001000b, 00010000b, 00100000b, 01000000b, 10000000b
